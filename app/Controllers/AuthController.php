b40c494885864fef981e98b23fa266a1411ee500
Edcel Celiz <edceliz01@gmail.com>
2018-05-21 21:37:34 +0800

    Added basic authentication

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -1,13 +1,31 @@
 <?php
   namespace App\Controllers;
 
+  use App\Models\User;
+
   class AuthController extends Controller {
     function getRegister($req, $res) {
       return $this->view->render($res, 'register.html');
     }
 
     function postRegister($req, $res) {
-      var_dump($req->getParsedBody());
-      die();
+      User::create([
+        'username' => $req->getParam('username'),
+        'password' => PASSWORD_HASH($req->getParam('password'), PASSWORD_DEFAULT)
+      ]);
+      return $res->withRedirect($this->router->pathFor('auth.get.login'));
+    }
+
+    function getLogin($req, $res, $args) {
+      return $this->view->render($res, 'login.html', ['status' => isset($args['status'])]);
+    }
+
+    function postLogin($req, $res) {
+      $user = User::where('username', $req->getParam('username'))->first();
+      if ($user) {
+        var_dump(password_verify($req->getParam('password'), $user->password));
+      } else {
+        return $res->withRedirect($this->router->pathFor('auth.get.login') . '/error');
+      }
     }
   }
--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,8 @@
 <?php
   $app->get('/', 'HomeController:index');
-  $app->get('/register', 'AuthController:getRegister');
 
-  $app->post('/register', 'AuthController:postRegister');
+  $app->get('/register', 'AuthController:getRegister');
+  $app->post('/register', 'AuthController:postRegister')->setName('auth.register');
+
+  $app->get('/login[/{status}]', 'AuthController:getLogin')->setName('auth.get.login');
+  $app->post('/login', 'AuthController:postLogin')->setName('auth.login');
